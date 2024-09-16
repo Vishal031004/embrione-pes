f1f249418f82a5f8e2351cb4b4b14357a0aa0552
ohnonyx <145856657+ohnonyx@users.noreply.github.com>
2024-09-16 11:34:49 +0530

    Update AboutTheEvent for kodikon-4

--- components/Kodikon-4/AboutTheEvent/AboutTheEvent.jsx
+++ components/Kodikon-4/AboutTheEvent/AboutTheEvent.jsx
@@ -40,12 +40,11 @@ const AboutTheEvent = () => {
             <div className="blue__gradient absolute md:top-[800px]  h-[350px] w-[350px] md:h-[400px] md:w-[450px] z-[-60] " />
             <div className="flex flex-col lg:flex-row items-center justify-center ">
                 <div className="p-6">
-                    <p className="text-white text-justify p-4 md:text-xl">Kodikon 3.0, the third edition of our 24-hour highly successful flagship national level hackathon, aims at developing creative solutions to real-world problems while also promoting friendly, healthy competition amongst peers.
+                    <p className="text-white text-justify p-4 md:text-xl">Kodikon 4.0, the fourth edition of our 24-hour highly successful flagship national level hackathon, aims at developing creative solutions to real-world problems while also promoting friendly, healthy competition amongst peers.
                         Open to all engineering undergraduates across the nation, this event ignites innovation, camaraderie, and healthy competition.
                         160+ participants will battle it out for the coveted prizes.
                     </p>
-                    <p className="text-white text-justify p-4 md:text-xl">Backed by comprehensive mentoring and continuous guidance from industry-renowned experts, Kodikon 3.0 provides a platform for attendees to be immersed in valuable industry insights, cultivate essential soft skills, expand networks, and refine technical prowess.
-                        The event strives to turn aspirations into achievements, seamlessly blending innovation with expertise to shape the leaders of tomorrow.</p>
+                    <p className="text-white text-justify p-4 md:text-xl">As a hackathon catered towards aspiring students, Kodikon 4.0 aims to offer a platform for them to showcase their existing skills while fostering growth through collaborative learning. With comprehensive mentoring and continuous guidance from renowned experts, attendees gain valuable industry exposure, resulting in the development of vital soft skills, expanded networks, and enhanced technical acumen.</p>
                 </div>
                 <div className="grid place-items-center">
                     <Lottie
