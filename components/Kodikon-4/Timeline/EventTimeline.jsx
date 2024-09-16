72ccbdaaaf49e85bce4a2d0889dfa741a6dfe0b2
ohnonyx <145856657+ohnonyx@users.noreply.github.com>
2024-09-16 20:18:54 +0530

    Update EventTimeline.jsx

--- components/Kodikon-4/Timeline/EventTimeline.jsx
+++ components/Kodikon-4/Timeline/EventTimeline.jsx
@@ -9,7 +9,7 @@ import { kodikonEvents } from '@/constants';
 const EventTimeline = () => {
   return (
     <div id="#timeline" className="flex flex-col w-full gap-y-9" >
-      <TitleComponent titleData="Kodikon 3.0 Timeline" id="timeline" />
+      <TitleComponent titleData="Kodikon 4.0 Timeline" id="timeline" />
       <VerticalTimeline lineColor='#fff'>
         {
           kodikonEvents?.map((event, index) => (
