69e2a7f16368c8f9146d204c8ea52769945d5356
Samved Suresh Jain <106664875+SamvedSama@users.noreply.github.com>
2024-09-16 13:11:10 +0530

    Update sponsors.js

--- components/Kodikon-4/sponsors.js
+++ components/Kodikon-4/sponsors.js
@@ -1,1 +1,62 @@
-
+export const sponsors_for_kodikon_4 = [
+    {
+      category: "Tech Sponsors",
+      categorySponsors: [
+        {
+          name: "Axure",
+          imageUrl: "/sponsors/axure.png",
+          sponsorWebsiteURL:""
+        },
+        {
+          name: "Sponsor 2",
+          imageUrl: "",
+          sponsorWebsiteURL:""
+        },
+        {
+          name: "Sponsor 3",
+          imageUrl: "",
+          sponsorWebsiteURL:""
+        },
+      ],
+    },
+    // {
+    //   category: "Category2",
+    //   categorySponsors: [
+    //     {
+    //       name: "Sponsor 1",
+    //       imageUrl: "",
+    //       sponsorWebsiteURL:""
+    //     },
+    //     {
+    //       name: "Sponsor 2",
+    //       imageUrl: "",
+    //       sponsorWebsiteURL:""
+    //     },
+    //     {
+    //       name: "Sponsor 3",
+    //       imageUrl: "",
+    //       sponsorWebsiteURL:""
+    //     },
+    //   ],
+    // },
+    // {
+    //   category: "Category3",
+    //   categorySponsors: [
+    //     {
+    //       name: "Sponsor 1",
+    //       imageUrl: "",
+    //       sponsorWebsiteURL:""
+    //     },
+    //     {
+    //       name: "Sponsor 2",
+    //       imageUrl: "",
+    //       sponsorWebsiteURL:""
+    //     },
+    //     {
+    //       name: "Sponsor 3",
+    //       imageUrl: "",
+    //       sponsorWebsiteURL:""
+    //     },
+    //   ],
+    // },
+  ];
