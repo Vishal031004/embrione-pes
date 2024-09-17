0aae2604a3d3e2d0f70529b710b781c4c06ca6cf
ohnonyx <145856657+ohnonyx@users.noreply.github.com>
2024-09-17 19:54:53 +0530

    Update EventTimeline.jsx

--- components/Kodikon-4/Timeline/EventTimeline.jsx
+++ components/Kodikon-4/Timeline/EventTimeline.jsx
@@ -4,7 +4,7 @@ import TitleComponent from '../HelperComponents/TitleComponent'
 import { VerticalTimeline } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css';
 import EventTimelineComponent from './EventTimelineComponent';
-import { kodikonEvents } from '@/constants';
+import { kodikon4Events } from '@/constants';
 
 const EventTimeline = () => {
   return (
@@ -12,7 +12,7 @@ const EventTimeline = () => {
       <TitleComponent titleData="Kodikon 4.0 Timeline" id="timeline" />
       <VerticalTimeline lineColor='#fff'>
         {
-          kodikonEvents?.map((event, index) => (
+          kodikon4Events?.map((event, index) => (
             <EventTimelineComponent key={index} index = {index} eventTitle={event.eventTitle} eventTimings={event.eventTimings} eventDescription={event.eventDescription} miscellaneous={event.miscellaneous} />
           ))
         }
