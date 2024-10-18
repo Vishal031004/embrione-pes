25dcff0b146879b0064816041f60acf2e848fbf1
Vishal031004 <vishal04p74@gmail.com>
2024-10-18 17:46:06 +0530

    removed space

--- components/Kodikon-4/sponsors.js
+++ components/Kodikon-4/sponsors.js
@@ -1,5 +1,16 @@
 export const sponsors_for_kodikon_4 = [
   {
+    category: "Title Sponsor",
+    categorySponsors: [
+      {
+        name: "SentinelOne",
+        imageUrl: "/Kodikon4/Sponsors/Title Sponsor/SentinelOne.png",
+        description:"Discover the power of AI-powered,enterprise-wide cybersecurity",
+        sponsorWebsiteURL: "https://www.sentinelone.com/platform/?utm_medium=paid-search&utm_source=google-paid&utm_campaign=apj-ind-en-g-s-brand&utm_term=Sentinelone&utm_campaignid=19538119170&ad_id=673518301908&gad_source=1&gclid=CjwKCAjw68K4BhAuEiwAylp3klHVuA-FdiiDOilJdnzf-y4S3rOdRJlmw8MMnMD46yMpQ3MgjzNQ9BoC0pIQAvD_BwE"
+      }
+    ]
+  },
+  {
     category: "Tech Sponsors",
     categorySponsors: [
       {
@@ -21,38 +32,7 @@ export const sponsors_for_kodikon_4 = [
         name: "Flatlogic",
         imageUrl: "/Kodikon4/Sponsors/Platform Partners/flatlogic.png",
         sponsorWebsiteURL: "https://flatlogic.com/"
-      }//,
-      // {
-      //   name: "Sponsor 2",
-      //   imageUrl: "",
-      //   sponsorWebsiteURL: ""
-      // },
-      // {
-      //   name: "Sponsor 3",
-      //   imageUrl: "",
-      //   sponsorWebsiteURL: ""
-      // }
-    ]
-  },
-  {
-    category: "Title Sponsors",
-    categorySponsors: [
-      {
-        name: "Sentinel One",
-        imageUrl: "/Kodikon4/Sponsors/Title Sponsor/SentinelOne.png",
-        description:"Discover the power of AI-powered,enterprise-wide cybersecurity",
-        sponsorWebsiteURL: "https://www.sentinelone.com/platform/?utm_medium=paid-search&utm_source=google-paid&utm_campaign=apj-ind-en-g-s-brand&utm_term=Sentinelone&utm_campaignid=19538119170&ad_id=673518301908&gad_source=1&gclid=CjwKCAjw68K4BhAuEiwAylp3klHVuA-FdiiDOilJdnzf-y4S3rOdRJlmw8MMnMD46yMpQ3MgjzNQ9BoC0pIQAvD_BwE"
-      }//,
-      // {
-      //   name: "Sponsor 2",
-      //   imageUrl: "",
-      //   sponsorWebsiteURL: ""
-      // },
-      // {
-      //   name: "Sponsor 3",
-      //   imageUrl: "",
-      //   sponsorWebsiteURL: ""
-      // }
+      }
     ]
   }
 ];
