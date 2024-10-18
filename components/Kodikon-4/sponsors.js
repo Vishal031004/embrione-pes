d3fc011eb90494f25dc7d98f63e05968999cd21a
Vishal031004 <vishal04p74@gmail.com>
2024-10-18 15:51:15 +0530

    added   sentinelOne

--- components/Kodikon-4/sponsors.js
+++ components/Kodikon-4/sponsors.js
@@ -33,5 +33,26 @@ export const sponsors_for_kodikon_4 = [
       //   sponsorWebsiteURL: ""
       // }
     ]
+  },
+  {
+    category: "Title Sponsors",
+    categorySponsors: [
+      {
+        name: "Sentinel One",
+        imageUrl: "/Kodikon4/Sponsors/Title Sponsor/SentinelOne.png",
+        description:"Discover the power of AI-powered,enterprise-wide cybersecurity",
+        sponsorWebsiteURL: "https://www.sentinelone.com/platform/?utm_medium=paid-search&utm_source=google-paid&utm_campaign=apj-ind-en-g-s-brand&utm_term=Sentinelone&utm_campaignid=19538119170&ad_id=673518301908&gad_source=1&gclid=CjwKCAjw68K4BhAuEiwAylp3klHVuA-FdiiDOilJdnzf-y4S3rOdRJlmw8MMnMD46yMpQ3MgjzNQ9BoC0pIQAvD_BwE"
+      }//,
+      // {
+      //   name: "Sponsor 2",
+      //   imageUrl: "",
+      //   sponsorWebsiteURL: ""
+      // },
+      // {
+      //   name: "Sponsor 3",
+      //   imageUrl: "",
+      //   sponsorWebsiteURL: ""
+      // }
+    ]
   }
 ];
